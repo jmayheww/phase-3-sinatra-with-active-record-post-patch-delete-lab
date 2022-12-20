1c65bc1637a2eb65f5bae19e556a3d1b652699ae
Joshua <joshsmayhew@gmail.com>
2022-12-20 15:26:01 +0700

    commit tree 71927a8f38ae00b343708f37d27449272ae0c472

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < Sinatra::Base
-  set default_content_type: "application/json"
-  
+  set default_content_type: 'application/json'
+
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
   end
-  
+
   get '/bakeries/:id' do
     bakery = Bakery.find(params[:id])
     bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods' do
+    BakedGood.all.to_json
   end
 
   get '/baked_goods/by_price' do
@@ -23,4 +27,34 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    ).to_json
+  end
+
+  patch '/bakeries/:id' do
+    # find bakery by id
+    selected_bakery = Bakery.find(params[:id])
+
+    # update selected bakery
+
+    selected_bakery.update(
+      name: params[:name]
+    )
+
+    # parse update as json and return
+
+    selected_bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good_to_be_deleted = BakedGood.find(params[:id])
+
+    baked_good_to_be_deleted.destroy
+
+    baked_good_to_be_deleted.to_json
+  end
 end
